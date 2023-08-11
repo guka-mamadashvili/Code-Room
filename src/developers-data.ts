60ca9eea25e5e89184d4604478548858893b89aa
guka_mmda18 <guka.mamada@gmail.com>
2023-08-12 03:14:40 +0400

    changed user giorgi to dachi

--- src/developers-data.ts
+++ src/developers-data.ts
@@ -1,8 +1,8 @@
 export const Developers: any = [
     {
         id: 1,
-        saxeli: "Giorgi",
-        lname: "Delibashvili",
+        saxeli: "Dachi",
+        lname: "Loseurashvili",
         imgUrl: "../../assets/giorgi.svg",
         description: "Giorgi is Web Developer from Georgia",
         info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo beatae dignissimos provident id delectus veritatis cum odio, magni quis quos fugiat? Ullam iusto, rerum quasi officiis odit perspiciatis mollitia. ",
