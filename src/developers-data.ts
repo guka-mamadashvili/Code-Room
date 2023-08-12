8a6065ea0e53a0ce3c0a8ed645801b0bdffeb09f
guka_mmda18 <guka.mamada@gmail.com>
2023-08-12 18:59:44 +0400

    updated developer prifile

--- src/developers-data.ts
+++ src/developers-data.ts
@@ -7,9 +7,13 @@ export const Developers: any = [
         description: "Dachi is Web Developer from Georgia",
         info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo beatae dignissimos provident id delectus veritatis cum odio, magni quis quos fugiat? Ullam iusto, rerum quasi officiis odit perspiciatis mollitia. ",
         university: "International Black Sea Univeristy",
+        univIcon: "../../assets/ibsu.png",
         education: "Computer Science",
         proSphere: "Full Stack Web Developer",
         skills: "HTML, CSS, JavaScript, Angular, SASS, NodeJS, MongoDB",
+        skill1iconUrl: "../../assets/html.png",
+        skill2iconUrl: "../../assets/css.png",
+        skill3iconUrl: "../../assets/js.png",
     },
 
     {
@@ -20,9 +24,13 @@ export const Developers: any = [
         description: "Mariami is Software Developer from Georgia",
         info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo beatae dignissimos provident id delectus veritatis cum odio, magni quis quos fugiat? Ullam iusto, rerum quasi officiis odit perspiciatis mollitia. ",
         university: "International Black Sea Univeristy",
+        univIcon: "../../assets/ibsu.png",
         education: "Computer Science",
         proSphere: "Software Develper",
         skills: "C++, Java, Python,",
+        skill1iconUrl: "../../assets/cpp.png",
+        skill2iconUrl: "../../assets/pyth.png",
+        skill3iconUrl: "../../assets/java.png",
     },
 
     {
@@ -33,9 +41,13 @@ export const Developers: any = [
         description: "Ana is Linux Administrator from Georgia",
         info: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo beatae dignissimos provident id delectus veritatis cum odio, magni quis quos fugiat? Ullam iusto, rerum quasi officiis odit perspiciatis mollitia. ",
         university: "International Black Sea Univeristy",
+        univIcon: "../../assets/ibsu.png",
         education: "Computer Science",
         proSphere: "Linux Administrator",
         skills: "SQL, NTPC, VI-EDITOR, SWITCHES",
+        skill1iconUrl: "../../assets/terminal.png",
+        skill2iconUrl: "../../assets/linux.png",
+        skill3iconUrl: "../../assets/sql.png",
     }
 ]
 
